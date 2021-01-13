5286787a9d3d254bbc88c275f8e4a6619a1f0591
michalurb8 <michalurb8@wp.pl>
2021-01-13 21:34:47 +0100

    populationsize is now solver attribute

--- Solver.py
+++ Solver.py
@@ -2,28 +2,28 @@ from Network import Network
 from Chromosome import Chromosome
 
 class Solver:
-    def __init__(self):
+    def __init__(self, populationSize):
         filename = './Data/sample.xml'
         # filename = './Data/polska.xml'
         self.network = Network(filename)
         self.paramPathNum = 2
         self.cards = [(10,2), (40,5), (100, 9)]
         self.population = []
-        self.log = []
+        self.populationSize = populationSize
+        self.populate(populationSize)
     
     def populate(self, count):
         minCardValue = sorted(self.cards)[0][0]
         maxCardCount = int(self.network.getMaxDemand() / minCardValue)
-        for _ in range(count): #TODO madrzejsza generacja
+        for _ in range(count):
             self.population.append(Chromosome(self.network.getDemandNum(), self.paramPathNum, len(self.cards), maxCardCount))
     
     def newGeneration(self):
         if self.population == []:
-            print("pusto")
-        # pick parent pairs
+            raise Exception("Empty population, cannot generate new members")
+        # pick parent pairs: turniejowo/progowo/ruletkowo
         # produce children
         # assign value to every child
-        # save children to log ( delete worst?? stala pamiec ale tracimy historie)
         # create new population
     
     def checkOsobnik(self, chromosome):
@@ -35,7 +35,8 @@ class Solver:
                 cards = sum(allele)
                 for link in path:
                     self.network.incrementFlow(link, cards)
+                    #TODO: jezeli przepelniony, return 0
+        return chromosome.getCost()
 
-solver = Solver()
-solver.populate(10)
+solver = Solver(5)
 print(solver.population[0].getMatrix())
